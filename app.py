4b4d52e72ea6f26a0b3f46c2a6917c21a76e7964
shinyena00 <syn77@pusan.ac.kr>
2026-08-26 19:54:59 +0900

    auth 통일

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from routes.auth import auth_bp
+from routes.auth import auth
 
 app = Flask(__name__)
 
-app.register_blueprint(auth_bp)
+app.register_blueprint(auth)
 if __name__ == "__main__":
     app.run(debug=True)
